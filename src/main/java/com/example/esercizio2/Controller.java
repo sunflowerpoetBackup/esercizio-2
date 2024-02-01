bd95f8356e7690d3206c148e520574c6119224c5
sunflowerpoet <anna.simionato747@gmail.com>
2024-02-01 16:24:17 +0100

    first

--- src/main/java/com/example/esercizio2/Controller.java
+++ src/main/java/com/example/esercizio2/Controller.java
@@ -1,12 +1,14 @@
 package com.example.esercizio2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v2")
 public class Controller {
-    @GetMapping("/v2/ciao/Lombardia")
+    @GetMapping("/ciao/Lombardia")
     public Saluto saluta(@RequestParam String nome) {
         String provincia = "Lombardia";
         String saluto = "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
--- src/main/java/com/example/esercizio2/Esercizio2Application.java
+++ src/main/java/com/example/esercizio2/Esercizio2Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Esercizio2Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Esercizio2Application.class, args);
 	}
